dc1e49aa85251256b793fc1ab7f979ea915cf50b
Martín Rodríguez <monse.developer@gmail.com>
2024-02-08 22:31:40 -0600

    update

--- Qualite-dArt/src/js/carusel.js
+++ Qualite-dArt/src/js/carusel.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
+  $(".customers-logos").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function () {
   $(".customer-logos").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
